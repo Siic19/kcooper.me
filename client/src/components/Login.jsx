d63adfee2738ded5ce387ac5483b2134494e7cca
Kelsey Cooper <kcoop@live.ca>
2018-04-26 11:40:07 -0700

    added graphql setup to client index.js, also added login component with working gql call

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,31 +1,65 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import LoginForm from './LoginForm';
+import React, { Component } from 'react'
+import { extendObservable } from 'mobx'
+import { observer } from 'mobx-react'
+import { Button, Input, Container, Header } from 'semantic-ui-react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
-class Login extends Component {
-  submit = (values) => {
-    let error = false;
-    if (!values.email || !values.password) {
-      error = true;
-      // this.props.loginBlankError(true);
-      console.log('error - blank');
-    }
-    if (!error) {
-      this.props.authenticateUser(values);
-    }
-  };
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+
+    extendObservable(this, {
+      email: '',
+      password: '',
+    })
+  }
+
+  onChange = (e) => {
+    const { name, value } = e.target
+    this[name] = value
+  }
+
+  onSubmit = async () => {
+    const { email, password } = this
+
+    const response = await this.props.mutate({
+      variables: { email, password },
+    })
+    
+    const { login } = response.data
+    localStorage.setItem('token', login)
+  }
 
   render() {
+    const { email, password } = this
     return (
-      <div>
-        <LoginForm onSubmit={this.submit} />
-      </div>
-    );
+      <Container text>
+        <Header as="h2">Login</Header>
+        <Input
+          name="email"
+          onChange={this.onChange}
+          value={email}
+          placeholder="Email"
+          fluid
+        />
+        <Input
+          name="password"
+          onChange={this.onChange}
+          value={password}
+          type="password"
+          placeholder="Password"
+          fluid
+        />
+        <Button onClick={this.onSubmit}>Submit</Button>
+      </Container>
+    )
   }
 }
 
-Login.propTypes = {
-  authenticateUser: PropTypes.func.isRequired,
-};
-
-export default Login;
+const loginMutation = gql`
+  mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password)
+  }
+`
+export default graphql(loginMutation)(observer(Login))
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,25 @@
 /* eslint-env browser */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './components/App'
+
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:3001/graphql" }),
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById('root'),
-);
-
+)
